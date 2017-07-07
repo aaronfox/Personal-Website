b854e691bba1b0fdbc0687274fa197debea1d440
aaronfox <aaron.m.fox3@gmail.com>
2017-07-07 18:25:38 -0400

    add resume and blog routes and navbar

--- index.js
+++ index.js
@@ -10,10 +10,18 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-    response.render('pages/index');
-
+app.get('/', function(req, res) {
+    res.render('pages/index');
 });
+
+app.get('/blog', function(req, res) {
+    res.render('pages/blog');
+});
+
+app.get('/resume', function(req, res) {
+    res.render('pages/resume');
+});
+
 
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
