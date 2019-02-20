1b34b6b25c3878f022de1c1afcfb44e767edab89
aaronfox <aaron.m.fox3@gmail.com>
2019-02-19 23:41:19 -0500

    fixed grammatical paranthetical error and spelling bee logic

--- public/js/spelling_bee_check.js
+++ public/js/spelling_bee_check.js
@@ -1,6 +1,7 @@
 var player_one_score = 0;
 var player_two_score = 0;
 var curr_player = 1;
+var has_checked_spelling = false;
 
 // TODO: Make sure user cannot repeatedly press Check Word button to increment both scores
 // TODO: Make sure same words will not repeat (will need to be implemented via API)
@@ -15,37 +16,39 @@ $(document).ready(function () {
         player_one_score = 0;
         player_two_score = 0;
         curr_player = 1;
+        has_checked_spelling = false;
+    });
+    $("#test_button").click(function () {
+        has_checked_spelling = false;
     });
     $("#check_spelling").click(function () {
         word = $("#secret-entry-word").text();
-        console.log("word == " + word);
         spelling_attempt = $("#spelling_attempt").val();
 
-        if (word.valueOf().toLowerCase().trim() == spelling_attempt.valueOf().toLowerCase().trim()) {
-            if (curr_player == 1) {
-                console.log("incrementing player_one_score");
-                player_one_score++;
-                $("#player_one_score").text(player_one_score);
-                $("#spelling-alert").text("Player 1 spelled " + spelling_attempt.valueOf().toLowerCase().trim() + " correctly!");
-                curr_player = 2;
+        if (has_checked_spelling == false) {
+            if (word.valueOf().toLowerCase().trim() == spelling_attempt.valueOf().toLowerCase().trim()) {
+                if (curr_player == 1) {
+                    player_one_score++;
+                    $("#player_one_score").text(player_one_score);
+                    $("#spelling-alert").text("Player 1 spelled " + spelling_attempt.valueOf().toLowerCase().trim() + " correctly!");
+                    curr_player = 2;
+                } else {
+                    player_two_score++;
+                    $("#player_two_score").text(player_two_score);
+                    $("#spelling-alert").text("Player 2 spelled " + spelling_attempt.valueOf().toLowerCase().trim() + " correctly!");
+                    curr_player = 1;
+                }
             } else {
-                player_two_score++;
-                $("#player_two_score").text(player_two_score);
-                $("#spelling-alert").text("Player 2 spelled " + spelling_attempt.valueOf().toLowerCase().trim() + " correctly!");
-                curr_player = 1;
+                if (curr_player == 1) {
+                    $("#spelling-alert").text("Player one incorrectly spelled " + word.valueOf().toLowerCase().trim() + " as " + spelling_attempt.valueOf().toLowerCase().trim() + ". :(");
+                    curr_player = 2;
+                } else {
+                    $("#spelling-alert").text("Player two incorrectly spelled " + word.valueOf().toLowerCase().trim() + " as " + spelling_attempt.valueOf().toLowerCase().trim() + ". :(");
+                    curr_player = 1;
+                }
             }
-        } else {
-            if (curr_player == 1) {
-                $("#spelling-alert").text("Player one incorrectly spelled " + word.valueOf().toLowerCase().trim() + " as " + spelling_attempt.valueOf().toLowerCase().trim() + ". :(");
-                curr_player = 2;
-            } else {
-                $("#spelling-alert").text("Player two incorrectly spelled " + word.valueOf().toLowerCase().trim() + " as " + spelling_attempt.valueOf().toLowerCase().trim() + ". :(");
-                curr_player = 1;
-            }
-            console.log("you are wrong!");
-            console.log(player_one_score);
-            console.log(player_two_score);
         }
+        has_checked_spelling = true;
     });
 
 });
