93446dfb73634141fed2d8bf2cbf23d479994680
aaronfox <aaron.m.fox3@gmail.com>
2021-03-01 21:29:14 -0500

    adding multi_game. This might break things

--- index.js
+++ index.js
@@ -60,6 +60,91 @@ app.get('/cameron', function(req, res) {
     res.render('pages/cameron');
 });
 
+// ======================================= Multi game logic =======================================
+// var server = require('http').Server(app);
+// // NOTE: changed this from original
+// var io = require('socket.io')(server);
+
+var server = app.listen(app.get('port'));
+var io = require('socket.io')(server);
+
+// Keep track of all players in game with players
+var players = {};
+
+// star variable keeps track of position of star collectibles
+var star = {
+    x: Math.floor(Math.random() * 700) + 50,
+    y: Math.floor(Math.random() * 500) + 50
+};
+// scores variable keeps track of both team's score
+var scores = {
+    blue: 0,
+    red: 0
+};
+
+io.on('connection', function (socket) {
+    console.log('a user connected');
+    // create a new player and add it to our players object
+    players[socket.id] = {
+        rotation: 0,
+        x: Math.floor(Math.random() * 700) + 50,
+        y: Math.floor(Math.random() * 500) + 50,
+        playerId: socket.id,
+        team: (Math.floor(Math.random() * 2) == 0) ? 'red' : 'blue'
+    };
+    // send the players object to the new player
+    socket.emit('currentPlayers', players);
+
+    // send the star object to the new player
+    socket.emit('starLocation', star);
+
+    // send the current scores
+    socket.emit('scoreUpdate', scores);
+
+    // update all other players of the new player
+    socket.broadcast.emit('newPlayer', players[socket.id]);
+
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
+        // remove this player from our players object
+        delete players[socket.id];
+        // emit a message to all players to remove this player
+        // NOTE: changed this from original
+        io.emit('disconnected', socket.id);
+    });
+
+    // when a player moves, update the player data
+    socket.on('playerMovement', function (movementData) {
+        players[socket.id].x = movementData.x;
+        players[socket.id].y = movementData.y;
+        players[socket.id].rotation = movementData.rotation;
+        // emit a message to all players about the player that moved
+        socket.broadcast.emit('playerMoved', players[socket.id]);
+    });
+
+    socket.on('starCollected', function () {
+        if (players[socket.id].team === 'red') {
+            scores.red += 10;
+        } else {
+            scores.blue += 10;
+        }
+        star.x = Math.floor(Math.random() * 700) + 50;
+        star.y = Math.floor(Math.random() * 500) + 50;
+        io.emit('starLocation', star);
+        io.emit('scoreUpdate', scores);
+    });
+});
+
+app.get('/multi_game', function (req, res) {
+    // res.render('pages/multi_game');
+    res.sendFile(__dirname + '/views/pages/multi_game.html');
+    // res.sendFile('pages/multi_game.html');
+
+});
+
+
+// ======================================= END Multi game logic =======================================
+
 // Snake Code for MicroComputers Class
 snakeScore = 0
 topScores = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
@@ -156,6 +241,6 @@ app.get('*', function (req, res) {
     res.status(404).send('OOF. 404. Sorry about that. Here\'s a Frenchie: (imagine a frenchie here)');
 });
 
-app.listen(app.get('port'), function() {
-    console.log('Node app is running on port', app.get('port'));
-});
+// app.listen(app.get('port'), function() {
+//     console.log('Node app is running on port', app.get('port'));
+// });
